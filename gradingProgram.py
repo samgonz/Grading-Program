12a622e4a0a83ec9623c9d5347351dc340b21f9b
samgonz <doomteam1@gmail.com>
2022-09-30 23:30:37 -0500

    Initial Commit

--- gradingProgram.py
+++ gradingProgram.py
@@ -1,1 +1,25 @@
+#Dictionary that lists students and their class scores.
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
 
+#Empty Dictionary to hold students overall grade
+student_grades = {}
+
+#Loops through the students scores and assigns it a grade description based off the students score.
+for i in student_scores:
+    if student_scores[i] >= 91:
+        student_grades[i] = 'Outstanding'
+    elif student_scores[i] >= 81:
+        student_grades[i] = 'Exceeds Expectations'
+    elif student_scores[i] >= 71:
+        student_grades[i] = 'Acceptable'
+    else:
+        student_grades[i] = 'Fail'
+        
+#Prints out the student_grades dictionary
+print(student_grades)
